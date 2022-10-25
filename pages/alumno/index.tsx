53a206e0324977f624aeae641959938e0f821241
iocampo <iocampo@ecosur.mx>
2022-10-24 21:43:08 -0500

    Se configuro el layout

---
+++ pages/alumno/index.tsx
@@ -0,0 +1,9 @@
+import MainLayout from '@shared/components/layouts/MainLayoutAlumno';
+const Example = () => {
+  return (
+    <MainLayout>
+      <h3>Holas como estas</h3>
+    </MainLayout>
+  );
+};
+export default Example;
