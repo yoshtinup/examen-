0fc32cec00421be285a845c5ddfd69aebee91115
Diego Cruz Aguilar <apoyoutic26@ecosur.mx>
2022-11-29 21:43:53 +0000

    internacionalizar la entrada de nombre con expresiones regurales, agregar boton para ver rapidamente el perfil del proceso de un alumno

--- src/modules/consejo_tutelar/submodules/evaluacion/types/index.ts
+++ src/modules/consejo_tutelar/submodules/evaluacion/types/index.ts
@@ -13,3 +13,21 @@ export interface EstudianteCT extends EstudianteGql {
   IdEstatusCT: number;
   LeyendaEstatusCT: string;
 }
+
+export interface Rechazados {
+  Rechazados: Rechazado[];
+}
+
+export interface Rechazado {
+  RazonRechazo: string;
+  Ronda: number;
+  RolQueRechazo: string;
+  Academico: Academico;
+}
+
+export interface Academico {
+  ApellidoPaterno: string;
+  ApellidoMaterno: string;
+  Nombre: string;
+  Grado: string;
+}
