0fe9c3099b240a3ff638cd560035f3f6b1a456f7
Diego Cruz Aguilar <dcrag@pm.me>
2022-11-29 10:54:33 -0600

    acceso a Servicios Escolares a consejo tutelar y seccion de eliminados

--- src/modules/consejo_tutelar/submodules/evaluacion/queries/index.ts
+++ src/modules/consejo_tutelar/submodules/evaluacion/queries/index.ts
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query';
 import { gql } from 'graphql-request';
 import { hasuraClient } from '@shared/queries';
-import { EstatusCTGql } from '../types';
+import { EstatusCTGql, Rechazados } from '../types';
 
 export function useGetEstudianteCTEstatus(matricula: number) {
   return useQuery<EstatusCTGql[]>(
@@ -23,6 +23,37 @@ export function useGetEstudianteCTEstatus(matricula: number) {
         { matricula }
       );
       return ConformacionCT;
+    }
+  );
+}
+
+export function useGetIntegrantesCTEliminados(matricula: number) {
+  return useQuery<Rechazados[]>(
+    ['ct-estudiante-rechazados', matricula],
+    async () => {
+      const { CT } = await hasuraClient.request(
+        gql`
+          query integrantesCTEliminados($matricula: Int!) {
+            CT: db18_CT_Conformacion(
+              where: { Matricula: { _eq: $matricula } }
+            ) {
+              Rechazados: db18_CT_IntegrantesEliminados {
+                RazonRechazo
+                Ronda
+                RolQueRechazo: Rol
+                Academico: db17_PersonalAcademico {
+                  ApellidoPaterno
+                  ApellidoMaterno
+                  Nombre: Nombre_s_
+                  Grado
+                }
+              }
+            }
+          }
+        `,
+        { matricula }
+      );
+      return CT;
     }
   );
 }
