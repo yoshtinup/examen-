66dbe9464aec38240b02cc70036467db11953245
Eder Toledo <edertoledon@gmail.com>
2023-02-08 11:52:13 -0600

    Changes for reject evaluation.
    Close http://utilitics.ecosur.mx:8087/infonomia/evaluacion-etica/frontcei/issues/104

--- src/modules/cei/submodules/comite/components/evaluador/RechazarEvaluacion.tsx
+++ src/modules/cei/submodules/comite/components/evaluador/RechazarEvaluacion.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -7,62 +7,69 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Alert from '@mui/material/Alert';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
-import { AlertMessageProps, RechazarProps } from '../../__generated__/globalTypes'
-import DataService from '../../services/data'
-import { useAppSelector } from '../../hooks'
+import {
+  AlertMessageProps,
+  RechazarProps,
+} from '../../__generated__/globalTypes';
+import DataService from '../../services/data';
+import { useRecoilState } from 'recoil';
+import { alumnoAtom } from '../../store/slices/alumno';
 
 /**
  * Curpo de confirmacion de rechazar evaluacion
-* @returns
-*/
+ * @returns
+ */
 export default function BodyRechazarEvaluacion() {
-
-  const { alumno } = useAppSelector((state) => state.alumno)
+  const [alumno] = useRecoilState(alumnoAtom);
   const [motivo, setMotivo] = React.useState('');
   const [alert, setAlert] = React.useState<AlertMessageProps | null>(null);
 
-  const handleChangeMotivo = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleChangeMotivo = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setMotivo(event.target.value as string);
-  }
+  };
 
   // Enviar a la api el aviso que se rechazazo la evaluacion
-  function handleAction(){
+  function handleAction() {
     const rechazar: RechazarProps = {
-      idPropuesta: alumno.idFormulariosRespuestas,
-      tesis: alumno.tesis,
-      observacion: motivo
-    }
+      idPropuesta: alumno.alumno.idFormulariosRespuestas,
+      tesis: alumno.alumno.tesis,
+      observacion: motivo,
+    };
 
     DataService.setRechazar(rechazar)
       .then((response: any) => {
         setAlert({
-          severity: "success",
-          message: response.data
-        })
+          severity: 'success',
+          message: response.data,
+        });
       })
       .catch((e: any) => {
-        console.log(e.response.data)
         setAlert({
-          severity: "warning",
-          message: e.response.data
-        })
+          severity: 'warning',
+          message: 'No se pudo registrar el rechazo de evaluación',
+        });
       });
   }
 
   return (
     <>
       <DialogContent>
-        <DialogContentText>
-          ¿Desea rechazar la evaluación?
-        </DialogContentText>
-          <FormControl sx={{ m: 1, width: '100%' }}>
-            <FormLabel component="legend">Motivo</FormLabel>
-            <TextareaAutosize
-              aria-label="agregar motivo"
-              minRows={3}
-              placeholder="Agregar motivo"
-              onChange={handleChangeMotivo}
-            />
+        <DialogContentText>¿Desea rechazar la evaluación?</DialogContentText>
+        <Alert severity="warning">
+          Confirme el rechazo de evaluación en caso de tener algún conflicto de
+          intereses o describa la razón por la cual no desea participar en la
+          evaluación de esta propuesta
+        </Alert>
+        <FormControl sx={{ m: 1, width: '100%' }}>
+          <FormLabel component="legend">Motivo</FormLabel>
+          <TextareaAutosize
+            aria-label="agregar motivo"
+            minRows={3}
+            placeholder="Agregar motivo"
+            onChange={handleChangeMotivo}
+          />
         </FormControl>
       </DialogContent>
       <DialogActions>
@@ -70,7 +77,13 @@ export default function BodyRechazarEvaluacion() {
           Rechazar
         </Button>
       </DialogActions>
-      {alert ? <Alert variant="filled" severity={alert.severity}>{alert.message}</Alert> : <></> }
+      {alert ? (
+        <Alert variant="filled" severity={alert.severity}>
+          {alert.message}
+        </Alert>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
