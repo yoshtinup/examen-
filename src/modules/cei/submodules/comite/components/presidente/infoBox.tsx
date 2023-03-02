f64c3de1b18178c9d07dd96e1dda0de5d293ecae
Eder Toledo <edertoledon@gmail.com>
2023-03-02 10:17:39 -0600

    Hiden part of message
    Related http://utilitics.ecosur.mx:8087/infonomia/plan-de-trabajo/issues/7

--- src/modules/cei/submodules/comite/components/presidente/infoBox.tsx
+++ src/modules/cei/submodules/comite/components/presidente/infoBox.tsx
@@ -3,25 +3,29 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 type InfoBoxProps = {
- label: string
-}
+  label: string;
+};
 
 // Caja informativa
-const InfoBox: React.FC<InfoBoxProps>  = ({label}) => {
+const InfoBox: React.FC<InfoBoxProps> = ({ label }) => {
   return (
-    <Box sx={{
-      display: 'flex',
-      backgroundColor: 'success.main',
-      color: '#f2f2f2',
-      height: '80px',
-      width: '100px',
-      padding: '10px',
-      textAlign: 'center',
-      alignItems: 'center',
-      alignContent: 'center',
-    }} >
-        <Typography component="div" variant="h6" >{label}</Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        backgroundColor: 'success.main',
+        color: '#f2f2f2',
+        height: '80px',
+        width: '150px',
+        padding: '10px',
+        textAlign: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+      }}
+    >
+      <Typography component="div" variant="h6">
+        {label}
+      </Typography>
     </Box>
   );
 };
-export default InfoBox
+export default InfoBox;
