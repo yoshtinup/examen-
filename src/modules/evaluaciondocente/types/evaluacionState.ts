073ad41aee00ee1bed7d11bb73d1365f75091844
etoledo <edtoledo@ecosur.mx>
2023-03-21 16:09:32 -0600

    Change interface from same data from send

--- src/modules/evaluaciondocente/types/evaluacionState.ts
+++ src/modules/evaluaciondocente/types/evaluacionState.ts
@@ -1,75 +1,75 @@
 export interface DatosActividades {
-    nombre: string
-    profesores: Profesore[]
-  }
-  
-  export interface Profesore {
-    idProfesores: number
-    name: string
-  }
+  nombre: string;
+  profesores: Profesore[];
+}
+
+export interface Profesore {
+  idProfesores: number;
+  name?: string;
+}
 
 export interface Actividades {
-    IdMateriasOfertaAnual: string
-    Matricula: string
-    profesores: Profesore[]
-    planeacionDelCurso: PlaneacionDelCurso
-    valoracionDelCurso: ValoracionDelCurso
-  }
-  
-  export interface Profesore {
-    idProfesores: number
-    name: string
-    respuestas: Respuestas
-  }
-  
-  export interface Respuestas {
-    selects: Selects
-    textAreas: TextAreas
-  }
-  
-  export interface Selects {
-    P_II_5: string
-    P_II_6: string
-    P_II_7: string
-    P_II_8: string
-    P_III_9: string
-    P_III_10: string
-    P_III_11: string
-    P_IV_12: string
-    P_IV_13: string
-    P_IV_14: string
-    P_IV_15: string
-    P_V_16: string
-    P_V_17: string
-    P_VI_18_1: string
-    P_VI_18_2: string
-    P_VI_18_3: string
-    P_VI_18_4: string
-    P_VI_18_5: string
-    P_VI_18_6: string
-    P_VI_18_7: string
-    P_VI_18_8: string
-    P_VI_19_1: string
-    P_VI_19_2: string
-    P_VI_19_3: string
-    P_VI_19_4: string
-    P_VI_20: string
-    P_VI_21: string
-  }
-  
-  export interface TextAreas {
-    Valoracion_P_3: string
-    Valoracion_P_4: string
-  }
-  
-  export interface PlaneacionDelCurso {
-    P_I_1: string
-    P_I_2: string
-    P_I_3: string
-    P_I_4: string
-  }
-  
-  export interface ValoracionDelCurso {
-    Valoracion_P_1: string
-    Valoracion_P_2: string
-  }
+  idMateriasOfertaAnual: number;
+  matricula?: string;
+  profesores: Profesore[];
+  planeacionDelCurso: PlaneacionDelCurso;
+  valoracionDelCurso: ValoracionDelCurso;
+}
+
+export interface Profesore {
+  idProfesores: number;
+  name?: string;
+  respuestas: Respuestas;
+}
+
+export interface Respuestas {
+  selects: Selects;
+  textAreas: TextAreas;
+}
+
+export interface Selects {
+  p_II_5: number;
+  p_II_6: number;
+  p_II_7: number;
+  p_II_8: number;
+  p_III_9: number;
+  p_III_10: number;
+  p_III_11: number;
+  p_IV_12: number;
+  p_IV_13: number;
+  p_IV_14: number;
+  p_IV_15: number;
+  p_V_16: number;
+  p_V_17: number;
+  p_VI_18_1: number;
+  p_VI_18_2: number;
+  p_VI_18_3: number;
+  p_VI_18_4: number;
+  p_VI_18_5: number;
+  p_VI_18_6: number;
+  p_VI_18_7: number;
+  p_VI_18_8: number;
+  p_VI_19_1: number;
+  p_VI_19_2: number;
+  p_VI_19_3: number;
+  p_VI_19_4: number;
+  p_VI_20: number;
+  p_VI_21: number;
+}
+
+export interface TextAreas {
+  valoracion_P_3: string;
+  valoracion_P_4: string;
+}
+
+export interface PlaneacionDelCurso {
+  p_I_1: number;
+  p_I_2: number;
+  p_I_3: number;
+  p_I_4: number;
+}
+
+export interface ValoracionDelCurso {
+  valoracion_P_1: string;
+  valoracion_P_2: string;
+}
