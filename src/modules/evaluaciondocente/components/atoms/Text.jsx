862037baf2617bc618978b5e5bf4cd8a837e3c60
etoledo <edtoledo@ecosur.mx>
2023-03-29 15:19:23 -0600

    Add quetions and select options

--- src/modules/evaluaciondocente/components/atoms/Text.jsx
+++ src/modules/evaluaciondocente/components/atoms/Text.jsx
@@ -67,8 +67,188 @@ export const ValoracionQuestions = [
   },
 ];
 
-export const doc1 = 'Eduardo Suarez Morales';
+export const preguntasEvaluacionADocentes = [
+  {
+    seccion: 'Estrategias de enseñanza-aprendizaje',
+    key: 'II',
+    descripcion:
+      'Técnicas y métodos para facilitar la adquisición de conocimientos, habilidades y actitudes. ',
+    preguntas: [
+      {
+        key: 'p_II_5',
+        text: '1.- La persona docente colabora en el desarrollo de mis habilidades de investigar o plantear y resolver problemas',
+      },
+      {
+        key: 'p_II_6',
+        text: '2.- La persona docente a través de la asignatura, busca que reflexione sobre las necesidades sociales o ambientales de mi entorno. ',
+      },
+      {
+        key: 'p_II_7',
+        text: '3.- La persona docente enriquece el aprendizaje al realimentar actividades grupales, tareas, exámenes o exposiciones y, sus comentarios me han apoyado en la reflexión sobre aciertos y desaciertos. ',
+      },
+      {
+        key: 'p_II_8',
+        text: '4.- La persona docente propicia que relacione la teoría con ejemplos, experiencias o situaciones prácticas.',
+      },
+    ],
+  },
+  {
+    seccion: 'Ambiente de aprendizaje',
+    key: 'III',
+    descripcion:
+      'Interacción académica entre estudiantes y el personal docente ',
+    preguntas: [
+      {
+        key: 'p_III_9',
+        text: '5.- La persona docente crea ambientes de aprendizaje variados. ',
+      },
+      {
+        key: 'p_III_10',
+        text: '6.- La forma en que la persona docente imparte la asignatura me ha estimulado a generar ideas, soluciones y aserciones propias. ',
+      },
+      {
+        key: 'p_III_11',
+        text: '7.- La persona docente genera un ambiente adecuado para la libre expresión de las ideas. ',
+      },
+    ],
+  },
+  {
+    seccion: 'Evaluación del aprendizaje',
+    key: 'IV',
+    descripcion:
+      'Claridad y coherencia en las estrategias y criterios de evaluación, éstas apoyan y favorecen el aprendizaje. ',
+    preguntas: [
+      {
+        key: 'p_IV_12',
+        text: '8.- La persona docente proporciona criterios específicos para realizar las actividades de aprendizaje. ',
+      },
+      {
+        key: 'p_IV_13',
+        text: '9.- Los productos generados por las actividades de aprendizaje realizadas son considerados como parte de la acreditación de la asignatura. ',
+      },
+      {
+        key: 'p_IV_14',
+        text: '10.- Las evaluaciones de las actividades de aprendizaje se desarrollan acorde a los criterios proporcionados ',
+      },
+      {
+        key: 'p_IV_15',
+        text: '11.- La persona docente muestra apertura para la corrección de errores de apreciación en la evaluación. ',
+      },
+    ],
+  },
+  {
+    seccion: 'Tecnologías de información y comunicación',
+    key: 'V',
+    descripcion:
+      'Manejo de las tecnologías de información y comunicación como herramienta cognitiva. ',
+    preguntas: [
+      {
+        key: 'p_V_16',
+        text: '12.- La persona docente utiliza las tecnologías de información y comunicación de acuerdo con las necesidades de los estudiantes y el programa de la asignatura.',
+      },
+      {
+        key: 'p_V_17',
+        text: '13.- La persona docente promueve el uso de las tecnologías de información y comunicación para la realización de las actividades académicas.',
+      },
+    ],
+  },
+  {
+    seccion: 'Ética y valores',
+    key: 'VI',
+    descripcion: 'Con base en el Código de conducta de ECOSUR. ',
+    subsection: [
+      {
+        key: 'etica',
+        descripcion:
+          'La persona docente trata con respeto a los y las estudiantes, sin importar:',
+        preguntas: [
+          { key: 'p_VI_18_1', text: 'Su formación disciplinaria' },
+          { key: 'p_VI_18_2', text: 'Su origen étnico' },
+          { key: 'p_VI_18_3', text: 'Su estado civil' },
+          { key: 'p_VI_18_4', text: 'Su edad' },
+          { key: 'p_VI_18_5', text: 'Su orientación sexual' },
+          { key: 'p_VI_18_6', text: 'Su condición de salud' },
+          { key: 'p_VI_18_7', text: 'Su nacionalidad' },
+          { key: 'p_VI_18_8', text: 'Su forma de vida' },
+        ],
+      },
+      {
+        key: 'aula',
+        descripcion: 'La persona docente dentro y fuera del aula:',
+        preguntas: [
+          { key: 'p_VI_19_1', text: 'Utiliza un lenguaje respetuoso' },
+          { key: 'p_VI_19_2', text: 'Evita hacer burlas' },
+          {
+            key: 'p_VI_19_3',
+            text: 'Evita hacer comentarios o preguntas incómodas de contenido sexual',
+          },
+          {
+            key: 'p_VI_19_4',
+            text: 'Evita hacer comentarios o preguntas relacionadas con la vida privada de las y los estudiantes',
+          },
+          {
+            key: 'p_VI_20',
+            text: 'Fomenta, reconoce y valora el trabajo y las aportaciones de los y las estudiantes dentro y fuera del aula',
+          },
+          {
+            key: 'p_VI_21',
+            text: 'Respeta las diferencias de opinión y privilegia el diálogo en situaciones de conflicto',
+          },
+        ],
+      },
+      {
+        key: 'sugerencia',
+        descripcion: '',
+        preguntas: [
+          {
+            key: 'valoracion_P_3',
+            text: '¿Qué sugieres al personal docente para mejorar la asignatura?',
+            type: 'textArea',
+          },
+          {
+            key: 'valoracion_P_4',
+            text: 'Utiliza este espacio para ampliar tus respuestas a las preguntas de la sección de Ética y valores',
+            type: 'textArea',
+          },
+        ],
+      },
+    ],
+  },
+];
 
-export const doc2 = 'Manuel Elias Gutierrez';
+export const opcionesEvaluacion = [
+  { key: 0, text: '' },
+  { key: 5, text: 'Bastante bien, Muy satisfecho/a' },
+  { key: 4, text: 'Bien, Bastante satisfecho/a' },
+  { key: 3, text: 'Regular, Satisfecho/a' },
+  { key: 2, text: 'Mal, Poco satisfecho/a' },
+  { key: 1, text: 'Muy mal, Muy insatisfecho/a' },
+];
 
-export const doc3 = 'Martha Elena Valdez Moreno';
+export const totalPreguntasEvaluacionDocente =
+  preguntasEvaluacionADocentes.reduce(
+    (acumulador, seccion) => {
+      if (seccion.preguntas) {
+        acumulador.totalTextArea += seccion.preguntas.filter(
+          pregunta => pregunta.type === 'textArea'
+        ).length;
+        acumulador.totalSelect += seccion.preguntas.filter(
+          pregunta => !pregunta.type || pregunta.type !== 'textArea'
+        ).length;
+      }
+      if (seccion.subsection) {
+        seccion.subsection.forEach(subseccion => {
+          if (subseccion.preguntas) {
+            acumulador.totalTextArea += subseccion.preguntas.filter(
+              pregunta => pregunta.type === 'textArea'
+            ).length;
+            acumulador.totalSelect += subseccion.preguntas.filter(
+              pregunta => !pregunta.type || pregunta.type !== 'textArea'
+            ).length;
+          }
+        });
+      }
+      return acumulador;
+    },
+    { totalTextArea: 0, totalSelect: 0 }
+  );
