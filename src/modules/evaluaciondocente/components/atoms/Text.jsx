4e0ee54675c4be7abb25596754857afa36826b3f
etoledo <edtoledo@ecosur.mx>
2023-03-30 14:27:21 -0600

    Change key name for questions
    Related http://utilitics.ecosur.mx:8087/infonomia/plan-de-trabajo/issues/6

--- src/modules/evaluaciondocente/components/atoms/Text.jsx
+++ src/modules/evaluaciondocente/components/atoms/Text.jsx
@@ -75,20 +75,24 @@ export const preguntasEvaluacionADocentes = [
       'Técnicas y métodos para facilitar la adquisición de conocimientos, habilidades y actitudes. ',
     preguntas: [
       {
-        key: 'p_II_5',
+        id: 'p_II_5',
         text: '1.- La persona docente colabora en el desarrollo de mis habilidades de investigar o plantear y resolver problemas',
+        required: true,
       },
       {
-        key: 'p_II_6',
+        id: 'p_II_6',
         text: '2.- La persona docente a través de la asignatura, busca que reflexione sobre las necesidades sociales o ambientales de mi entorno. ',
+        required: true,
       },
       {
-        key: 'p_II_7',
+        id: 'p_II_7',
         text: '3.- La persona docente enriquece el aprendizaje al realimentar actividades grupales, tareas, exámenes o exposiciones y, sus comentarios me han apoyado en la reflexión sobre aciertos y desaciertos. ',
+        required: true,
       },
       {
-        key: 'p_II_8',
+        id: 'p_II_8',
         text: '4.- La persona docente propicia que relacione la teoría con ejemplos, experiencias o situaciones prácticas.',
+        required: true,
       },
     ],
   },
@@ -99,16 +103,19 @@ export const preguntasEvaluacionADocentes = [
       'Interacción académica entre estudiantes y el personal docente ',
     preguntas: [
       {
-        key: 'p_III_9',
+        id: 'p_III_9',
         text: '5.- La persona docente crea ambientes de aprendizaje variados. ',
+        required: true,
       },
       {
-        key: 'p_III_10',
+        id: 'p_III_10',
         text: '6.- La forma en que la persona docente imparte la asignatura me ha estimulado a generar ideas, soluciones y aserciones propias. ',
+        required: true,
       },
       {
-        key: 'p_III_11',
+        id: 'p_III_11',
         text: '7.- La persona docente genera un ambiente adecuado para la libre expresión de las ideas. ',
+        required: true,
       },
     ],
   },
@@ -119,20 +126,24 @@ export const preguntasEvaluacionADocentes = [
       'Claridad y coherencia en las estrategias y criterios de evaluación, éstas apoyan y favorecen el aprendizaje. ',
     preguntas: [
       {
-        key: 'p_IV_12',
+        id: 'p_IV_12',
         text: '8.- La persona docente proporciona criterios específicos para realizar las actividades de aprendizaje. ',
+        required: true,
       },
       {
-        key: 'p_IV_13',
+        id: 'p_IV_13',
         text: '9.- Los productos generados por las actividades de aprendizaje realizadas son considerados como parte de la acreditación de la asignatura. ',
+        required: true,
       },
       {
-        key: 'p_IV_14',
+        id: 'p_IV_14',
         text: '10.- Las evaluaciones de las actividades de aprendizaje se desarrollan acorde a los criterios proporcionados ',
+        required: true,
       },
       {
-        key: 'p_IV_15',
+        id: 'p_IV_15',
         text: '11.- La persona docente muestra apertura para la corrección de errores de apreciación en la evaluación. ',
+        required: true,
       },
     ],
   },
@@ -143,12 +154,14 @@ export const preguntasEvaluacionADocentes = [
       'Manejo de las tecnologías de información y comunicación como herramienta cognitiva. ',
     preguntas: [
       {
-        key: 'p_V_16',
+        id: 'p_V_16',
         text: '12.- La persona docente utiliza las tecnologías de información y comunicación de acuerdo con las necesidades de los estudiantes y el programa de la asignatura.',
+        required: true,
       },
       {
-        key: 'p_V_17',
+        id: 'p_V_17',
         text: '13.- La persona docente promueve el uso de las tecnologías de información y comunicación para la realización de las actividades académicas.',
+        required: true,
       },
     ],
   },
@@ -162,37 +175,49 @@ export const preguntasEvaluacionADocentes = [
         descripcion:
           'La persona docente trata con respeto a los y las estudiantes, sin importar:',
         preguntas: [
-          { key: 'p_VI_18_1', text: 'Su formación disciplinaria' },
-          { key: 'p_VI_18_2', text: 'Su origen étnico' },
-          { key: 'p_VI_18_3', text: 'Su estado civil' },
-          { key: 'p_VI_18_4', text: 'Su edad' },
-          { key: 'p_VI_18_5', text: 'Su orientación sexual' },
-          { key: 'p_VI_18_6', text: 'Su condición de salud' },
-          { key: 'p_VI_18_7', text: 'Su nacionalidad' },
-          { key: 'p_VI_18_8', text: 'Su forma de vida' },
+          {
+            id: 'p_VI_18_1',
+            text: 'Su formación disciplinaria',
+            required: true,
+          },
+          { id: 'p_VI_18_2', text: 'Su origen étnico', required: true },
+          { id: 'p_VI_18_3', text: 'Su estado civil', required: true },
+          { id: 'p_VI_18_4', text: 'Su edad', required: true },
+          { id: 'p_VI_18_5', text: 'Su orientación sexual', required: true },
+          { id: 'p_VI_18_6', text: 'Su condición de salud', required: true },
+          { id: 'p_VI_18_7', text: 'Su nacionalidad', required: true },
+          { id: 'p_VI_18_8', text: 'Su forma de vida', required: true },
         ],
       },
       {
         key: 'aula',
         descripcion: 'La persona docente dentro y fuera del aula:',
         preguntas: [
-          { key: 'p_VI_19_1', text: 'Utiliza un lenguaje respetuoso' },
-          { key: 'p_VI_19_2', text: 'Evita hacer burlas' },
           {
-            key: 'p_VI_19_3',
+            id: 'p_VI_19_1',
+            text: 'Utiliza un lenguaje respetuoso',
+            required: true,
+          },
+          { id: 'p_VI_19_2', text: 'Evita hacer burlas', required: true },
+          {
+            id: 'p_VI_19_3',
             text: 'Evita hacer comentarios o preguntas incómodas de contenido sexual',
+            required: true,
           },
           {
-            key: 'p_VI_19_4',
+            id: 'p_VI_19_4',
             text: 'Evita hacer comentarios o preguntas relacionadas con la vida privada de las y los estudiantes',
+            required: true,
           },
           {
-            key: 'p_VI_20',
+            id: 'p_VI_20',
             text: 'Fomenta, reconoce y valora el trabajo y las aportaciones de los y las estudiantes dentro y fuera del aula',
+            required: true,
           },
           {
-            key: 'p_VI_21',
+            id: 'p_VI_21',
             text: 'Respeta las diferencias de opinión y privilegia el diálogo en situaciones de conflicto',
+            required: true,
           },
         ],
       },
@@ -201,14 +226,16 @@ export const preguntasEvaluacionADocentes = [
         descripcion: '',
         preguntas: [
           {
-            key: 'valoracion_P_3',
+            id: 'valoracion_P_3',
             text: '¿Qué sugieres al personal docente para mejorar la asignatura?',
             type: 'textArea',
+            required: true,
           },
           {
-            key: 'valoracion_P_4',
+            id: 'valoracion_P_4',
             text: 'Utiliza este espacio para ampliar tus respuestas a las preguntas de la sección de Ética y valores',
             type: 'textArea',
+            required: true,
           },
         ],
       },
