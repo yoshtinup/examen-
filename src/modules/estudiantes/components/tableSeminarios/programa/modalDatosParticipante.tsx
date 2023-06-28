c0a4d378caccd980719bdcf36b4b08751328f1b4
etoledo <edtoledo@ecosur.mx>
2023-06-28 16:20:14 -0600

    Related http://utilitics.ecosur.mx:8087/infonomia/posgrado/sistema-de-informacion-posgrado/issues/83

---
+++ src/modules/estudiantes/components/tableSeminarios/programa/modalDatosParticipante.tsx
@@ -0,0 +1,53 @@
+import { Box, Button, Modal } from '@mui/material';
+import { useState } from 'react';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '100%',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  height: '80vh',
+};
+
+const ModalDatosParticipante = ({
+  idParticipante,
+}: {
+  idParticipante: number;
+}) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={handleOpen} variant="outlined">
+        Ver detalles
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <iframe
+            src={
+              'https://aplicaciones.ecosur.mx/app/funcionalidades-sip/datos-participantes-asignatura-6490b028e8023e77eb94dec9?idProfesor=' +
+              idParticipante +
+              '&embed=true'
+            }
+            width="100%"
+            height="100%"
+            style={{ border: 'none' }}
+          ></iframe>
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalDatosParticipante;
