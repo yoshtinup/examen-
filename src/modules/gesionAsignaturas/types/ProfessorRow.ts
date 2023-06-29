cb9202cba241a48867fa86a53b69988f0a0d095d
etoledo <edtoledo@ecosur.mx>
2023-06-29 11:07:00 -0600

    Complement feature
    Related http://utilitics.ecosur.mx:8087/infonomia/posgrado/sistema-de-informacion-posgrado/issues/70
    
    http://utilitics.ecosur.mx:8087/infonomia/posgrado/sistema-de-informacion-posgrado/issues/83
    
    http://utilitics.ecosur.mx:8087/infonomia/posgrado/sistema-de-informacion-posgrado/issues/86#
    
    http://utilitics.ecosur.mx:8087/infonomia/plan-de-trabajo/issues/23

---
+++ src/modules/gesionAsignaturas/types/ProfessorRow.ts
@@ -0,0 +1,8 @@
+interface ProfessorRow {
+  nombre: string;
+  id: string;
+  tipo_participacion: string;
+  unidad: string;
+  porcentaje_participacion: number | 'N/A';
+  enlaces?: JSX.Element;
+}
