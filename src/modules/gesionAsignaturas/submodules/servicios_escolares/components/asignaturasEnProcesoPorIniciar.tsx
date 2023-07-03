515423a3c804462ca436f594da6e1a4e51b8b942
iocampo <iocampo@ecosur.mx>
2023-07-02 20:49:00 -0600

    Ajustes en textos

--- src/modules/gesionAsignaturas/submodules/servicios_escolares/components/asignaturasEnProcesoPorIniciar.tsx
+++ src/modules/gesionAsignaturas/submodules/servicios_escolares/components/asignaturasEnProcesoPorIniciar.tsx
@@ -1,29 +1,39 @@
-import { Grid } from "@mui/material";
-import { useGetListaAsignaturasProcesoIniciar } from "@shared/queries/listaAsignaturas";
-import TablaAsignaturas from "./tablaAsignaturas";
-import { AsignaturaGql } from "@shared/types/listaAsignaturas";
+import { Grid } from '@mui/material';
+import { useGetListaAsignaturasProcesoIniciar } from '@shared/queries/listaAsignaturas';
+import TablaAsignaturas from './tablaAsignaturas';
+import { AsignaturaGql } from '@shared/types/listaAsignaturas';
 
 const AsignaturasEnProcesoPorIniciar = () => {
   const { isLoading, isError, data } = useGetListaAsignaturasProcesoIniciar();
   return (
     <>
-      <Grid container spacing={2} style={{padding:"50px"}}>
-        { isLoading ? (
+      <Grid container spacing={2} style={{ padding: '20px 50px' }}>
+        {isLoading ? (
           <>Cargando</>
+        ) : isError ? (
+          <>Error</>
         ) : (
-          isError ? (
-            <>Error</>
-          ) : (
-            <TablaAsignaturas asignaturas={data.Asignaturas.sort((a: AsignaturaGql, b: AsignaturaGql) => {
-              if (Date.parse(a.Datos.FechasAsignatura.FechaInicio) > Date.parse(b.Datos.FechasAsignatura.FechaInicio)) return -1;
-              if (Date.parse(a.Datos.FechasAsignatura.FechaInicio) < Date.parse(b.Datos.FechasAsignatura.FechaInicio)) return 1;
-              return 0;
-            })} />
-          )
+          <TablaAsignaturas
+            asignaturas={data.Asignaturas.sort(
+              (a: AsignaturaGql, b: AsignaturaGql) => {
+                if (
+                  Date.parse(a.Datos.FechasAsignatura.FechaInicio) >
+                  Date.parse(b.Datos.FechasAsignatura.FechaInicio)
+                )
+                  return -1;
+                if (
+                  Date.parse(a.Datos.FechasAsignatura.FechaInicio) <
+                  Date.parse(b.Datos.FechasAsignatura.FechaInicio)
+                )
+                  return 1;
+                return 0;
+              }
+            )}
+          />
         )}
       </Grid>
     </>
   );
-}
+};
 
 export default AsignaturasEnProcesoPorIniciar;
