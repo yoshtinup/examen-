45950d6b691c186d91edd9de789a28dae7d5b7c9
oswaldo_L <Saul_Oswaldo_Lara_Gonzalez_SOLG@hotmail.com>
2023-08-10 14:02:55 -0600

    Primer commit

--- src/modules/evaluaciondocente/recoil/profesoresState.ts
+++ src/modules/evaluaciondocente/recoil/profesoresState.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil';
 import { Profesor } from '@modules/evaluaciondocente/types/evaluacionState';
 
+
 export const profesoresState = atom({
   key: 'profesoresState',
   default: [] as Profesor[],
